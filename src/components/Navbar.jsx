cc05f51a34754f7d319eff700e846876f0310a73
Mikael <dev.kov.mtl@gmail.com>
2024-11-23 14:09:03 -0500

    frontend - react tailwind menu design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,65 @@ const Navbar = () => {
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img src="/logo.png" alt="logo" className="w-8 h-8" />
+        <span>blog</span>
+      </div>
       {/* MOBILE MENU */}
-      <div className="md:hidden"></div>
+      <div className="md:hidden">
+        {/* MOBILE BTN */}
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <div className="flex flex-col gap-[5.4px]">
+            <div
+              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${
+                open && 'rotate-45'
+              }`}
+            ></div>
+            <div
+              className={`h-[3px] rounded-md w-6 bg-black transition-all ease-in-out ${
+                open && 'opacity-0'
+              }`}
+            ></div>
+            <div
+              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${
+                open && '-rotate-45'
+              }`}
+            ></div>
+          </div>
+        </div>
+
+        {/* MOBILE LIST */}
+        <div
+          className={`w-full h-full bg-[#e6e6ff] flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+            open ? '-right-0' : '-right-[100%]'
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </a>
+        </div>
+      </div>
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login 👋
+          </button>
+        </a>
+      </div>
     </div>
   );
 };
