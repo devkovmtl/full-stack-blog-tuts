eca74bf1000a931135080b7052d199847e000d8b
Mikael <dev.kov.mtl@gmail.com>
2024-11-24 08:59:37 -0500

    frontend - home page routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './index.css';
+import MainLayout from './layouts/MainLayout';
+import Homepage from './routes/Homepage';
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Homepage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/routes/Homepage.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+const Homepage = () => {
+  return (
+    <div className="mt-4 flex flex-col gap-4">
+      {/* BREADCRUMB */}
+      <div className="flex gap-4">
+        <Link to={'/'}>Home</Link>
+        <span>▪</span>
+        <span className="text-blue-800">Blogs and Articles</span>
+      </div>
+      {/* INTRODUCTION */}
+      <div className="flex items-center justify-between">
+        {/* titles */}
+        <div>
+          <h1 className="text-gray-800 text-2xl md:text-5xl lg:text-6xl font-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className="mt-8 text-md md:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            cupiditate itaque quibusdam perferendis dignissimos sapiente.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
