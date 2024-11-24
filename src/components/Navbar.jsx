954f3fbed3dffc74f0273d4ceca78b91a189c959
Mikael <dev.kov.mtl@gmail.com>
2024-11-24 11:43:49 -0500

    frontend - update nav with link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { IKImage } from 'imagekitio-react';
 import Image from './Image';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -45,28 +45,28 @@ const Navbar = () => {
             open ? '-right-0' : '-right-[100%]'
           }`}
         >
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">LinkAbout</Link>
+          <Link to="">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login 👋
             </button>
-          </a>
+          </Link>
         </div>
       </div>
       {/* DESKTOP MENU */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">LinkAbout</Link>
+        <Link to="">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
             Login 👋
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
